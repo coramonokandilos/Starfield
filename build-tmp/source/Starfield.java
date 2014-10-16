d061e5e5975832f14f7a07d356eac29eed0a763f
coramonokandilos <cora.monokandilos@gmail.com>
2014-10-16 10:11:17 -0700

    Made some cool changes

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] particlez;
 
-void setup()
+public void setup()
 {
 	size(700, 700);
 	background(0);
@@ -11,7 +27,7 @@ void setup()
 	}
 	particlez[particlez.length - 1] = new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	if((int)((NormalParticle)particlez[0]).dX == 350 && (int)((NormalParticle)particlez[0]).dY ==  350)
 	{
@@ -37,7 +53,7 @@ class NormalParticle implements Particle
 		{
 			dX = dX + Math.cos(dTheta)*dSpeed;
 			dY = dY + Math.sin(dTheta)*dSpeed;
-			dTheta += 0.01 ;
+			dTheta += 0.01f ;
 		}
 	public void show()
 		{
@@ -66,7 +82,7 @@ class OddballParticle implements Particle
 	{
 		dXX = dXX + Math.cos(dTheta2)*dSpeed2;
 		dYY = dYY + Math.sin(dTheta2)*dSpeed2;
-		dTheta2 += 0.01;
+		dTheta2 += 0.01f;
 	}
 	public void show()
 	{
@@ -76,3 +92,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
